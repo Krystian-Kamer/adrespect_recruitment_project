b4f83f4288678cef0b1577bd09f15a2421b3da0d
Krystian <texen24@gmail.com>
2023-08-18 21:38:56 +0200

    added bootstrap to project, created first section

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 // ************VARIABLES************
-
-const offerList = document.querySelector('.offer-list');
-const offerItems = document.querySelectorAll('.offer-item');
+const searchInput = document.querySelector('input');
+const searchBtn = document.querySelector('.btn-search');
 
 // ************FUNCTIONS************
 
-const showOffers = () => {
-  offerItems.forEach((offerItem) => {
-    offerItem.classList.toggle('offer-item-active');
-  });
+
+
+
+const showSearchInput = (e) => {
+  e.preventDefault()
+  if (searchInput.classList.contains('d-none')) {
+    searchInput.classList.remove('d-none');
+    searchInput.classList.add('d-flex');
+  } else if (searchInput.classList.contains('d-flex')) {
+    searchInput.classList.remove('d-flex');
+    searchInput.classList.add('d-none');
+  }
+  searchInput.value = '';
 };
 
 // ************LISTENERS************
 
-offerList.addEventListener('click', showOffers);
+searchBtn.addEventListener('click', showSearchInput);
