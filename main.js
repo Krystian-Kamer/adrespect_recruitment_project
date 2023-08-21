e15f4a45b8c1b83bc02a388f37ad840707243320
Krystian <texen24@gmail.com>
2023-08-21 20:44:30 +0200

    changed intro section and added function closeBurgerList

--- main.js
+++ main.js
@@ -2,15 +2,15 @@
 
 const searchInput = document.querySelector('input');
 const searchBtn = document.querySelector('.btn-search');
-const burgerIcon = document.querySelector('.navbar-toggler span')
-const hiddenList = document.querySelector('.hidden-list')
+const burgerIcon = document.querySelector('.navbar-toggler span');
+const burgerList = document.querySelector('.hidden-list');
 const spanYear = document.querySelector('.current-year span');
-
+const burgerLinks = document.querySelectorAll('.burger-nav-link');
 
 // ************FUNCTIONS************
 
 const showSearchInput = (e) => {
-  e.preventDefault()
+  e.preventDefault();
   if (searchInput.classList.contains('d-none')) {
     searchInput.classList.remove('d-none');
     searchInput.classList.add('d-flex');
@@ -25,25 +25,30 @@ const handleItemList = () => {
   if (burgerIcon.classList.contains('navbar-toggler-icon')) {
     burgerIcon.classList.remove('navbar-toggler-icon');
     burgerIcon.classList.add('btn-close');
-    hiddenList.classList.remove('d-none')
-    hiddenList.classList.add('d-flex')
+    burgerList.classList.remove('d-none');
+    burgerList.classList.add('d-flex');
   } else if (burgerIcon.classList.contains('btn-close')) {
-    burgerIcon.classList.remove('btn-close');
-    burgerIcon.classList.add('navbar-toggler-icon');
-    hiddenList.classList.remove('d-flex')
-    hiddenList.classList.add('d-none')
+    closeBurgerList();
   }
-}
+};
+
+const closeBurgerList = () => {
+  burgerIcon.classList.remove('btn-close');
+  burgerIcon.classList.add('navbar-toggler-icon');
+  burgerList.classList.remove('d-flex');
+  burgerList.classList.add('d-none');
+};
 
 const getCurrentYear = () => {
   const currentYear = new Date().getFullYear();
   spanYear.textContent = ` ${currentYear} `;
 };
 getCurrentYear();
+
 // ************LISTENERS************
 
 searchBtn.addEventListener('click', showSearchInput);
-burgerIcon.addEventListener('click', handleItemList)
-
-
-
+burgerIcon.addEventListener('click', handleItemList);
+burgerLinks.forEach((burgerLink) => {
+  burgerLink.addEventListener('click', closeBurgerList);
+});
