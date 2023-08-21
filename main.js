e305362cecca4f20b8c84e3d350c06d52f9ced66
Krystian <texen24@gmail.com>
2023-08-21 22:40:08 +0200

    created functions in projects section and make this section RWD

--- main.js
+++ main.js
@@ -6,10 +6,16 @@ const burgerIcon = document.querySelector('.navbar-toggler span');
 const burgerList = document.querySelector('.hidden-list');
 const spanYear = document.querySelector('.current-year span');
 const burgerLinks = document.querySelectorAll('.burger-nav-link');
+const container = document.querySelector('#container');
+const buttonMore = document.querySelector('.btn-more');
+
+const API_KEY = '36233552-776e28d27d3c6ccbd0cd51246';
+let page = 1;
 
 // ************FUNCTIONS************
 
 const showSearchInput = (e) => {
+  c;
   e.preventDefault();
   if (searchInput.classList.contains('d-none')) {
     searchInput.classList.remove('d-none');
@@ -38,6 +44,54 @@ const closeBurgerList = () => {
   burgerList.classList.remove('d-flex');
   burgerList.classList.add('d-none');
 };
+
+const loadImages = (page) => {
+  fetch(
+    `https://pixabay.com/api/?key=${API_KEY}&q=garden%20design&image_type=photo&per_page=20&page=${page}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      createImageCards(data);
+    });
+};
+
+const createImageCards = (data) => {
+  let imagesLoaded = 0;
+
+  for (let i = 0; i < data.hits.length; i++) {
+    let div = document.createElement('div');
+    div.className = 'grid-item';
+
+    let img = document.createElement('img');
+    img.src = data.hits[i].webformatURL;
+    img.alt = data.hits[i].tags;
+    img.onload = () => {
+      imagesLoaded++;
+
+      if (imagesLoaded === data.hits.length) {
+        initMasonry();
+      }
+    };
+
+    div.appendChild(img);
+    container.appendChild(div);
+  }
+};
+
+const initMasonry = () => {
+  const masonry = new Masonry(container, {
+    itemSelector: '.grid-item',
+    gutter: 10,
+  });
+};
+
+loadImages();
+
+buttonMore.addEventListener('click', () => {
+  console.log('first');
+  page++;
+  loadImages(page);
+});
 
 const getCurrentYear = () => {
   const currentYear = new Date().getFullYear();
